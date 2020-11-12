74c7e3c76bdc839d3005268af4d3806d086a13f2
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 13:30:40 +0300

    refactor database and server connection, shifted constants responsibility to config library

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const app = require('./app')
-const port = process.env.PORT || 5000
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const app = express()
 
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`)
-})
+const PORT = config.get('serverPort')
+const MONGO_URI = config.get('mongoURI')
+const MONGO_OPTIONS = config.get('mongoOptions')
+
+const start = async() => {
+  try {
+    await mongoose.connect(MONGO_URI, MONGO_OPTIONS)
+
+    app.listen(PORT, () => {
+      console.log(`Server started on http://localhost:${PORT}`)
+    })
+  } catch (error) {
+    console.log('The server or database failed')
+  }
+}
+
+start()
